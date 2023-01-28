b30d64dd49e7fe37e781c5ec456617c2041c374e
TeguhFauzi <teguhfauzi55@gmail.com>
2023-01-29 04:13:06 +0700

    23

--- assets/code.js
+++ assets/code.js
@@ -7,22 +7,22 @@ function getData() {
 
 
     if (nama == "") {
-        console.log("Nama tidak boleh Kosong.")
+        alert("Nama tidak boleh Kosong!")
 
     } else if (email == "") {
-        console.log("Email tidak boleh kosong!")
+        alert("Email tidak boleh kosong!")
     } else if (subject == "") {
-        console.log("Harus di isi!")
+        alert("Harus di isi!")
     } else if (tanggal == "") {
-        console.log("Harus di isi!")
+        alert("Harus di isi!")
     } else if (job == "") {
-        console.log("Dipilih seusai option!")
+        alert("Dipilih seusai option!")
     }
 
     const defaultEmail = "teguhfauzi55@gmail.com"
 
     let mailTo = document.createElement('a')
-    mailTo.href = `mailto:${defaultEmail}?subject=${subject}&body=Halo nama saya ${nama},saya seorang ${job}hari ini tanggal ${tanggal}tolong hubungin saya ${email}`
+    mailTo.href = `mailto:${defaultEmail}?subject=${subject}&body=Halo nama saya ${nama}, Saya seorang ${job}. Ini ${tanggal}, Tolong hubungi saya di ${email}`
     mailTo.click()
 
     let audience = {
